f9aea22b432db8d97bea6e6389ab5fa89a465e1c
andrew <horolskyandrey@gmail.com>
2024-02-24 17:15:09 +0300

    answer serializer

---
+++ app/serializers/answer_serializer.rb
@@ -0,0 +1,8 @@
+class AnswerSerializer
+  include JSONAPI::Serializer
+  attributes :user_id, :survey_id, :created_at
+
+  attribute :answer_data do |object|
+    object.answer_data.attributes.except("_id", "created_at", "updated_at")
+  end
+end
