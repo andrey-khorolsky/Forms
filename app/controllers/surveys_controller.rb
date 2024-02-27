a6a66f4eb39e00b8434655ee86aee9a0d145e352
andrew <horolskyandrey@gmail.com>
2024-02-28 00:38:09 +0300

    create action for survey controller

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -9,6 +9,17 @@ class SurveysController < ApplicationController
     render json: SurveySerializer.new(Survey.find(params[:id])).serializable_hash.to_json
   end
 
+  # POST /surveys
+  def create
+    survey = Survey.new(survey_params)
+
+    if survey.save
+      render json: SurveySerializer.new(survey).serializable_hash.to_json
+    else
+      render json: survey.errors, status: 422
+    end
+  end
+
   # DELETE /surveys/98e17521-56d6-4ecc-a903-091ff4a387c5
   def destroy
     survey = Survey.find_by(id: params[:id])
@@ -19,5 +30,11 @@ class SurveysController < ApplicationController
       survey.destroy
       render json: {}, status: 200
     end
+  end
+
+  private
+
+  def survey_params
+    params.require(:survey).permit(:name, :actived, :questions_count, questions: [:number, :type, :text])
   end
 end
--- app/models/survey.rb
+++ app/models/survey.rb
@@ -7,6 +7,15 @@ class Survey < ApplicationRecord
 
   has_many :answers, dependent: :destroy
 
+  def initialize(attributes)
+    questions = Question.new(questions_count: attributes[:questions_count], questions: attributes[:questions])
+
+    return unless questions.save
+    super(attributes.except(:questions, :questions_count))
+
+    update(question_id: questions.id.to_s)
+  end
+
   def question
     Question.find_by(id: question_id)
   rescue Mongoid::Errors::DocumentNotFound
