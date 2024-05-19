2347ff9c4966d049ab0a5042d87e60d3c4dc5616
andrew <horolskyandrey@gmail.com>
2024-05-19 17:31:31 +0300

    serialize anonymous answer

--- app/serializers/answer_serializer.rb
+++ app/serializers/answer_serializer.rb
@@ -1,11 +1,13 @@
 class AnswerSerializer
   include JSONAPI::Serializer
-  attributes :user_id, :survey_id, :created_at
+  attributes :survey_id, :created_at
+
+  attribute :user_id, if: proc { |object| !object.survey.anonymous }
 
   attribute :answer_data do |object|
     object.answer_data.attributes.except("_id", "created_at", "updated_at")
   end
 
-  belongs_to :user
+  belongs_to :user, if: proc { |object| !object.survey.anonymous }
   belongs_to :survey
 end
