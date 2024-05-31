050cfc233aa943ada5ddb160412bd79bbd43ffe3
andrew <horolskyandrey@gmail.com>
2024-06-01 00:53:12 +0300

    add participant model concern

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,17 +1,14 @@
 class Group < ApplicationRecord
   include Owner
   include Stored
+  include Participant
 
   validates :name, presence: true
 
   # get the members (users or groups)
-  has_many :group_members
+  has_many :group_members, dependent: :destroy
   has_many :member_users, through: :group_members, source: :member, source_type: "User"
   has_many :member_groups, through: :group_members, source: :member, source_type: "Group"
-
-  # get the groups the object belongs to
-  has_many :group_members_as_member, class_name: "GroupMember", as: :member
-  has_many :groups, through: :group_members_as_member
 
   # get the all members (users and groups)
   def members
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,14 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable,
     :jwt_authenticatable, jwt_revocation_strategy: self
   include Devise::JWT::RevocationStrategies::JTIMatcher
+
   include Owner
+  include Participant
 
   validates :name, presence: true
 
   has_many :answers
-
-  has_many :group_members, as: :member
-  has_many :groups, through: :group_members
 
   def answers_for_survey(survey_id)
     answers.where(survey_id: survey_id)
