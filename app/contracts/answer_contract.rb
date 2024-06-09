09c4ec96a796a9136d34400aaf87c3b556346225
andrew <horolskyandrey@gmail.com>
2024-06-09 14:30:23 +0300

    fix v2 answer contract schema

--- app/contracts/answer_contract.rb
+++ app/contracts/answer_contract.rb
@@ -1,19 +1,10 @@
 class AnswerContract < Dry::Validation::Contract
-  OneAnswer = Dry::Schema.Params do
-    required(:number).filled(:integer)
-    required(:result).filled(:string)
-  end
-
-  MultipleAnswer = Dry::Schema.Params do
-    required(:number).filled(:integer)
-    required(:result).array(:string)
-  end
-
   params do
     required(:answers_count).filled(:integer)
     optional(:answer_time).filled(:string)
     required(:answer_data).array(:hash) do
-      OneAnswer.or(MultipleAnswer)
+      required(:number).filled(:integer)
+      required(:result).filled
     end
   end
 
