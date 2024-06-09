dab6864c23f6c074d9bbf6beb62b854abda68cd1
andrew <horolskyandrey@gmail.com>
2024-06-09 14:51:27 +0300

    fix v3 answer contract schema

--- app/contracts/answer_contract.rb
+++ app/contracts/answer_contract.rb
@@ -4,13 +4,13 @@ class AnswerContract < Dry::Validation::Contract
     optional(:answer_time).filled(:string)
     required(:answer_data).array(:hash) do
       required(:number).filled(:integer)
-      required(:result).filled
+      required(:result)
     end
   end
 
-  rule(:answer_data) do
-    unless value.map { _1[:number] } == (1..values[:answers_count]).to_a
-      key.failure("Expected answers count and actual answers count not matches")
-    end
-  end
+  # rule(:answer_data) do
+  #   unless value.map { _1[:number] } == (1..values[:answers_count]).to_a
+  #     key.failure("Expected answers count and actual answers count not matches")
+  #   end
+  # end
 end
