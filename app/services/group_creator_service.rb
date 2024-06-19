ec42b72e7ac66f8cd8daae2d96ea61b168d79a5c
andrew <horolskyandrey@gmail.com>
2024-06-19 23:10:29 +0300

    from user to owner

--- app/services/group_creator_service.rb
+++ app/services/group_creator_service.rb
@@ -2,7 +2,7 @@ class GroupCreatorService
   def self.create_group_from_params(params, owner)
     ActiveRecord::Base.transaction do
       new_group = Group.create!(params)
-      AddPermissionService.add_admin(new_group, user)
+      AddPermissionService.add_admin(new_group, owner)
     end
 
     new_group
