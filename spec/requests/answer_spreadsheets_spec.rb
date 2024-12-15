be0347f572b5474a45ad5422229c88ce4c8edaa1
andrew <horolskyandrey@gmail.com>
2024-12-15 21:56:44 +0300

    upd answer spreadsheets and statistics

--- spec/requests/answer_spreadsheets_spec.rb
+++ spec/requests/answer_spreadsheets_spec.rb
@@ -9,58 +9,32 @@ RSpec.describe "answer_spreadsheets", type: :request do
   before { sign_in user }
   after { DatabaseCleaner.clean }
 
-  path "/surveys/{survey_id}/answer_spreadsheet{format}" do
+  path "/surveys/{survey_id}/answer_spreadsheet" do
     parameter name: "survey_id", in: :path, type: :string, description: "survey's ID"
-    parameter name: "format", in: :path, type: :string, description: "format of response"
+    parameter name: :format, in: :query, type: :string, description: "format of file (xlsx, csv, ods, xml, yaml)"
 
     let(:survey_id) { survey.id }
-
+    let(:format) { "xml" }
+    
     get("show answer_spreadsheet") do
-      response(200, "send xlsx file") do
-        produces "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-
-        let(:format) { ".xlsx" }
-
-        run_test! do |response|
-          expect(response.content_type).to eq "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-        end
-      end
-
-      response(200, "send csv file") do
-        produces "text/csv"
-
-        let(:format) { ".csv" }
-
-        run_test! do |response|
-          expect(response.content_type).to eq "text/csv"
-        end
-      end
-
-      response(200, "send xml file") do
-        produces "text/xml"
-
-        let(:format) { ".xml" }
-
-        run_test! do |response|
-          expect(response.content_type).to eq "text/xml"
-        end
+      response(200, "sends file") do
+        produces "text/xml", "text/csv", "application/yaml", "application/vnd.oasis.opendocument.spreadsheet",
+          "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        
+        run_test!
       end
 
       response(400, "incorrect format") do
+        let(:format) { "asd" }
         produces "application/json; charset=utf-8"
 
-        let(:format) { ".example" }
-
-        run_test! do |response|
-          expect(response.content_type).to eq "application/json; charset=utf-8"
-        end
+        run_test!
       end
 
       response(404, "survey not found") do
-        produces "application/json; charset=utf-8"
-
         let(:survey_id) { "bad_id" }
-        let(:format) { ".xlsx" }
+
+        produces "application/json; charset=utf-8"
 
         run_test! do |response|
           expect(response.content_type).to eq "application/json; charset=utf-8"
--- spec/requests/answer_statistics_spec.rb
+++ spec/requests/answer_statistics_spec.rb
@@ -3,23 +3,42 @@ require "swagger_helper"
 RSpec.describe "answer_statistics", type: :request do
   let(:user) { create(:user) }
   let(:survey) { create(:survey, question_mongo_id: question.id) }
-  let!(:answer) { create(:answer, :with_answer_data, survey_id: survey.id, user: user, answer_mongo_id: answer_data.id) }
+  let!(:answer) do
+    create(:answer, :with_answer_data,
+    survey_id: survey.id,
+    user: user,
+    answer_mongo_id: answer_data.id,
+    created_at: creation_date)
+  end
   let(:answer_data) { create(:answer_datum) }
-  let!(:question) { create(:question) }
+  let(:question) { create(:question) }
   let(:role) { create(:role, :manager) }
   let!(:permission) { create(:permission, role: role, owner: user, entity: survey) }
+  let(:creation_date) { Date.new(2024, 12, 15)}
 
   before { sign_in user }
 
   path "/surveys/{survey_id}/answer_statistics" do
-    parameter name: "survey_id", in: :path, type: :string, description: "survey_id"
-    parameter name: "date_from", in: :body, type: :string, description: "from this date statistics will be collected"
-    parameter name: "date_to", in: :body, type: :string, description: "to this date statistics will be collected"
-    parameter name: "except_question_numbers", in: :body, type: :string, description: "numbers of questions, that are not included in statistics"
+    parameter name: "survey_id", in: :path, type: :string, description: "id of survey"
+    parameter name: "date_from", in: :query, type: :string, description: "from this date statistics will be collected"
+    parameter name: "date_to", in: :query, type: :string, description: "to this date statistics will be collected"
+    parameter name: "except_question_numbers", in: :query, type: :string, description: "numbers of questions, that are not included in statistics"
 
     get("show answer_statistic") do
+      let(:date_from) { creation_date }
+      let(:date_to) { creation_date }
+      let(:except_question_numbers) { "1,2,3" }
+
       response(200, "successful") do
         let(:survey_id) { survey.id }
+        produces "application/json; charset=utf-8"
+
+        run_test!
+      end
+
+      response(404, "not found") do
+        let(:survey_id) { "invalid-id" }
+        produces "application/json; charset=utf-8"
 
         run_test!
       end
